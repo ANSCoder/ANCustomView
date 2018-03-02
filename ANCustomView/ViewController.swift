6cfe33011423279e3226d86ab522d09f0f47ede2
Anand Nimje <anand@anands-macbook-pro.local>
2018-03-02 14:56:55 +0530

    View Animation Functionality.

--- ANCustomView/ViewController.swift
+++ ANCustomView/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewCircle: ANCustomView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func actionAnimateView(_ sender: UIButton) {
+        //Animation for View here
+        viewCircle.AnimateView()
     }
-
 
 }
 
